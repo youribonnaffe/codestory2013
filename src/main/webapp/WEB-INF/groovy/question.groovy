997bc0d880980bd5d0aa0764f0239e89c4eb429a
Youri <youribm@gmail.com>
2013-01-12 18:13:03 +0100

    keep sending me enonce1

--- src/main/webapp/WEB-INF/groovy/question.groovy
+++ src/main/webapp/WEB-INF/groovy/question.groovy
@@ -7,4 +7,5 @@ switch (question){
     case "Es tu abonne a la mailing list(OUI/NON)" : sout.print "OUI"; break;
     case "Est ce que tu reponds toujours oui(OUI/NON)" : sout.print "NON"; break;
     case "Es tu pret a recevoir une enonce au format markdown par http post(OUI/NON)" : sout.print "OUI"; break;
+    case "As tu bien recu le premier enonce(OUI/NON)" : sout.print "NON"; break;
 }
